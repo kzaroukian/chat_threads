72a7d09b755ebdc8de874e5519650b1140395251
Kaylin <kzaroukian@gmail.com>
2018-11-12 23:22:59 -0500

    final version - fixed print again

--- chat_server.c
+++ chat_server.c
@@ -647,7 +647,7 @@ void* handleclient(void* arg) {
         printf("\n");
 
         printf("Encrypted Text:\n");
-        BIO_dump_fp(stdout, no_iv2,s);
+        BIO_dump_fp(stdout, no_iv2,s-20);
         printf("\n");
 
         int decryptedans_len = decrypt(no_iv2, ans_encrypt_len, get_clients_vals->symmetric_keys[s_index], iv2, decrypted_ans);
